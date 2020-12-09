658798f2f7a36d1530d18005ca9b39b45c243366
Mažeika Jonas <jonas.mazeika@mif.stud.vu.lt>
2020-12-09 17:13:14 +0200

    Kas cia vyksta?

--- app/src/main/java/com/example/myapplication/ParkingLot.java
+++ app/src/main/java/com/example/myapplication/ParkingLot.java
@@ -1,11 +1,18 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -14,44 +21,133 @@ import com.google.firebase.database.ValueEventListener;
 
 public class ParkingLot extends AppCompatActivity {
 
+    private Button pl1;
+    private Button pl2;
+
+    private  Firebase mRootRef;
+    private  FirebaseAuth mFirebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.parking_lot);
-       // basicReadWrite();
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        Firebase.setAndroidContext(this);
+        mRootRef = new Firebase("https://userdata-da93a.firebaseio.com/ParkingLot");
+
+        Button logout = (Button) findViewById(R.id.logoutB);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if(mFirebaseUser != null){
+                    mFirebaseAuth.signOut();
+                    Toast.makeText(ParkingLot.this, "Logout is successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(ParkingLot.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(ParkingLot.this, "You are not currently logged in!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        pl1 = (Button) findViewById(R.id.pl1);
+        pl1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(1==1){
+                    FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                    if (mFirebaseUser != null) {
+                        // User is signed in.
+
+                        Firebase childRef = mRootRef.child("Pirma");
+                        childRef.setValue("123");
+                        //validate();
+                        Toast.makeText(ParkingLot.this, "Prisijungta?", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // No user is signed in.
+                        Toast.makeText(ParkingLot.this, "Neprisijungta?", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            };
+        });
+
+
+        pl2 = (Button) findViewById(R.id.pl2);
+        pl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("ParkingLot");
+                Toast.makeText(ParkingLot.this, "Vapse veikia tas void", Toast.LENGTH_SHORT).show();
+
+                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Toast.makeText(ParkingLot.this, "Vapse veikia tascccc", Toast.LENGTH_SHORT).show();
+                        if(snapshot.exists()){
+                            String m = snapshot.child("Antrute").child("nzn").getValue(String.class);
+                            Toast.makeText(ParkingLot.this, m, Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+//                reference.addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                        Toast.makeText(ParkingLot.this, "O cia?", Toast.LENGTH_SHORT).show();
+//
+//                        String a = snapshot.getValue().toString();
+//                        String b = snapshot.getChildren().toString();
+//
+//                        Toast.makeText(ParkingLot.this, a, Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(ParkingLot.this, b, Toast.LENGTH_SHORT).show();
+//
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) {
+//
+//                    }
+//                });
+            }
+        });
 
     }
 
-   /* public void basicReadWrite() {
-        // [START write_message]
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
-        // [END write_message]
+    private void validate(){
 
-        // [START read_message]
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Toast.makeText(ParkingLot.this, value, Toast.LENGTH_SHORT).show();
+      DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("ParkingLot");
+        Toast.makeText(ParkingLot.this, "Vapse veikia tas void", Toast.LENGTH_SHORT).show();
 
-            }
+        reference.addValueEventListener(new ValueEventListener() {
+          @Override
+          public void onDataChange(@NonNull DataSnapshot snapshot) {
+              Toast.makeText(ParkingLot.this, "O cia?", Toast.LENGTH_SHORT).show();
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                //Log.w(TAG, "Failed to read value.", error.toException());
-                Toast.makeText(ParkingLot.this, "Nepaejo", Toast.LENGTH_SHORT).show();
+              String a = snapshot.getValue().toString();
+          String b = snapshot.getChildren().toString();
+
+              Toast.makeText(ParkingLot.this, a, Toast.LENGTH_SHORT).show();
+              Toast.makeText(ParkingLot.this, b, Toast.LENGTH_SHORT).show();
+
+          }
+
+          @Override
+          public void onCancelled(@NonNull DatabaseError error) {
+
+          }
+      });
+    };
 
 
-            }
-        });
-        }
-    */
 }
